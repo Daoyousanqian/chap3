1e34355df354d9c0a91f66040d36a464726ff933
Daoyousanqian <shengquan.wang@campus.tu-berlin.de>
2020-03-04 13:09:44 -0800

    complete the first one

--- main.c
+++ main.c
@@ -7,19 +7,22 @@ int main(){
 	
 	int targetNum;
 	float appro_pre = 1.0;
-	float appro_aft;
+	float appro_aft = 1.0;
 	while ((scanf("%d", &targetNum)) == 1 ){
 		
-		while(appro_aft != appro_pre){
+		if(targetNum < 0)
+			printf("please enter the number that greater than 0");
+		else{
+		
+			do {
+				
+				appro_pre = appro_aft;     // assgin the appropriate new value to last one 
+				appro_aft = (appro_pre + targetNum/appro_pre)/2;   // update the new value
+				
+			}while(appro_aft != appro_pre);
 			
-			appro_aft = (appro_pre + targetNum/appro_pre)/2;
-			appro_pre = appro_aft;
-			
-			
+			printf("the number of appro square root is: %f\n", appro_aft);
 		}
-		
-		printf("the number of appro square root is: %f\n", appro_aft)
-		
 	}
 	
 	return 1;
