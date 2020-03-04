d883251016ceb73bf7955ed48e34bd50b866beb8
Daoyousanqian <shengquan.wang@campus.tu-berlin.de>
2020-03-04 13:39:58 -0800

    add triangle function

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 #include<stdlib.h>
 
 void prime(int num);
+void triangle(int a, int b , int c);
 // the main function is used to get the appropriate value square root 
 
 int main(){
 	
-	int targetNum;
+	int targetNum, targetNum1, targetNum2;
 	float appro_pre = 1.0;
 	float appro_aft = 1.0;
-	while ((scanf("%d", &targetNum)) == 1 ){
+	while ((scanf("%d %d %d", &targetNum, &targetNum1, &targetNum2)) == 1 ){
 		
 		if(targetNum < 0)
 			printf("please enter the number that greater than 0");
@@ -21,15 +22,16 @@ int main(){
 				appro_aft = (appro_pre + targetNum/appro_pre)/2;   // update the new value
 				
 			}while(appro_aft != appro_pre);
-	        
+	        triangle(targetNum,targetNum1,targetNum2); 
 			prime(targetNum);
 			printf("the number of appro square root is: %f\n", appro_aft);
+			
 		}
 	}
 	
 	return 1;
 }
-
+// used to calculate the prime 
 void prime(int num){
 	
 	int count = 0;
@@ -48,4 +50,27 @@ void prime(int num){
 	}
 	else
 		printf("it is not a prime\n");
+}
+
+// this function is used to calculate the triagle property; 
+void triangle(int a, int b , int c){
+	
+	if( a+b>c && a+c > b && b+c >a ){
+		
+		if(a == b || a== c || b == c)
+		{
+			if(a == b == c)
+				printf("equilateral triangle");
+			else
+				printf("isosceles triangle");
+			
+		}
+		else
+			printf("ordinary triangle");
+	}
+	else
+		printf("this is not a triangle \n");
+	
+	
+	
 }
