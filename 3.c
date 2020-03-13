3b3aea039ee6497a9170af7d53e1afafc4ae4460
Daoyousanqian <shengquan.wang@campus.tu-berlin.de>
2020-03-13 13:32:25 -0700

    add link list

--- 3.c
+++ 3.c
@@ -554,7 +554,28 @@ creat_subassy_rec (int n_parts){
 	
 void creat_link_list(){
 	
+	Node *previous;
+	Node *new;
+	/*
+	** Look for the right place by walking down the list
+	** until we reach a node whose value is greater than
+	** or equal to the new value.
+	*/
+	while( current->value < new_value ){
+		previous = current;
+		current = current->link;
+	}
 	
+	new = (Node *)malloc( sizeof( Node ) );
+	if( new == NULL )
+	return FALSE;
+	new->value = new_value;
+	/*
+	** Insert the new node into the list, and return TRUE.
+	*/
+	new->link = current;
+	previous->link = new;
+	return TRUE;
 	
 }
 
